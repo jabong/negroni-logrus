ce8232ff5cf3d9907244557e539bdccaa2804714
apoorvamoghey <apoorva.moghey@jabong.com>
2015-05-27 12:43:04 +0530

    update gitignore|add logging to syslog only in live mode

--- middleware.go
+++ middleware.go
@@ -37,13 +37,15 @@ func (l *Middleware) ServeHTTP(rw http.ResponseWriter, r *http.Request, next htt
 	config := conf.GetConfig()
 	if config.Env == "dev" {
 		l.Logger.WithFields(logrus.Fields{
-			"method":  r.Method,
-			"request": r.RequestURI,
-			"remote":  r.RemoteAddr,
+			"method":         r.Method,
+			"request":        r.RequestURI,
+			"remote":         r.RemoteAddr,
+			"X-Jabong-Reqid": r.Header.Get("X-Jabong-Reqid"),
+			"X-Jabong-Tid":   r.Header.Get("X-Jabong-Tid"),
 		}).Info("started handling request")
-
-		log.Info(fmt.Sprintf("started handling request: method=%s remote=%s request=%s", r.Method, r.RemoteAddr, r.RequestURI))
 	}
+	msg := fmt.Sprintf("started handling request: method=%s remote=%s request=%s X-Jabong-Reqid=%v X-Jabong-Tid=%v", r.Method, r.RemoteAddr, r.RequestURI, r.Header.Get("X-Jabong-Reqid"), r.Header.Get("X-Jabong-Tid"))
+	log.Info(msg)
 	next(rw, r)
 	latency := time.Since(start)
 	res := rw.(negroni.ResponseWriter)
@@ -56,13 +58,14 @@ func (l *Middleware) ServeHTTP(rw http.ResponseWriter, r *http.Request, next htt
 			"text_status": http.StatusText(res.Status()),
 			"took":        latency,
 			fmt.Sprintf("measure#%s.latency", l.Name): latency.Nanoseconds(),
+			"X-Jabong-Reqid":                          r.Header.Get("X-Jabong-Reqid"),
+			"X-Jabong-Tid":                            r.Header.Get("X-Jabong-Tid"),
 		}).Info("completed handling request")
 	}
-
-	msg := fmt.Sprintf("completed handling request: measure#%s.latency=%d method=%s remote=%s request=%s status=%d text_status=%s took=%s", l.Name, latency.Nanoseconds(), r.Method, r.RemoteAddr, r.RequestURI, res.Status(), http.StatusText(res.Status()), latency)
+	msg = fmt.Sprintf("completed handling request: measure#%s.latency=%d method=%s remote=%s request=%s status=%d text_status=%s took=%s X-Jabong-Reqid=%v X-Jabong-Tid=%v", l.Name, latency.Nanoseconds(), r.Method, r.RemoteAddr, r.RequestURI, res.Status(), http.StatusText(res.Status()), latency, r.Header.Get("X-Jabong-Reqid"), r.Header.Get("X-Jabong-Tid"))
 	if res.Status() == http.StatusOK {
 		log.Info(msg)
 	} else {
-		log.Crit(msg)
+		log.Err(msg)
 	}
 }
